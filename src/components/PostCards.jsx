c067f5d40dff97de4e242b49f81b01971032adeb
Apoorva1102 <apoorvamaurya1102@gmail.com>
2026-07-27 11:48:47 +0530

    minor changes

--- src/components/PostCards.jsx
+++ src/components/PostCards.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import appwriteService from  "../appwrite/config";
 import {Link} from 'react-router-dom';
-const preview = appwriteService.getFilePreview(featuredImage);
+
+export default function PostCards({$id, title, featuredImage}){
+
+    const preview = appwriteService.getFilePreview(featuredImage);
 
 console.log(preview);
 console.log(typeof preview);
 
-export default function PostCards({$id, title, featuredImage}){
     return(
         <Link to={`/post/${$id}`}>
             <div className="w-full rounded-xl bg-slate-900 text-white p-4">
