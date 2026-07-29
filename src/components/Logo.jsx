5c1e3df971823bf61cdb659a4f2bbf87ae34b477
Apoorva1102 <apoorvamaurya1102@gmail.com>
2026-07-29 12:47:46 +0530

    made minor changes to UI

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import bazel from "../assets/bazel.svg"
+import moon from "../assets/moon.png"
 export default function Logo({width = '100px'}){
     return(
         <div>
-            <img src={bazel} alt="Blog"
+            <img src={moon} alt="Blog"
             className="w-12 h-12 text-indigo-500"></img>
         </div>
     )
